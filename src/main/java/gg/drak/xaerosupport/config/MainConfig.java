b9b7fd74c089a80361f9ce6e94ad486d17b43447
Drak <business@drak.gg>
2026-01-24 04:10:49 -0500

    feature: auto-regenerate server id if needed

--- src/main/java/gg/drak/xaerosupport/config/MainConfig.java
+++ src/main/java/gg/drak/xaerosupport/config/MainConfig.java
@@ -11,32 +11,97 @@ public class MainConfig extends SimpleConfiguration {
         super("config.yml", XaeroSupport.getInstance(), true);
     }
 
+    /**
+     * Initialize the configuration by loading necessary values.
+     */
     @Override
     public void init() {
-        getXaeroServerId();
+        checkAndGenerateNewId(); // Reloads the configuration automatically. (Sets if not set yet.)
     }
 
+    /**
+     * Get the Xaero server ID from the configuration.
+     * @return The Xaero server ID.
+     */
     public int getXaeroServerId() {
         reloadResource();
 
         return getOrSetDefault("ids.server", getRandomServerId());
     }
 
+    /**
+     * Get the world map channel name from the configuration.
+     * @return The world map channel name.
+     */
     public String getWorldMapChannel() {
         reloadResource();
 
         return getOrSetDefault("channels.world-map.main", "xaeroworldmap:main");
     }
 
+    /**
+     * Get the minimap channel name from the configuration.
+     * @return The minimap channel name.
+     */
     public String getMinimapChannel() {
         reloadResource();
 
         return getOrSetDefault("channels.minimap.main", "xaerominimap:main");
     }
 
+    /**
+     * Set the Xaero server ID in the configuration.
+     * @param id The Xaero server ID to set.
+     */
+    public void setServerId(int id) {
+        write("ids.server", id);
+    }
+
+    /**
+     * Set the world map channel name in the configuration.
+     * @param channel The world map channel name to set.
+     */
+    public void setWorldMapChannel(String channel) {
+        write("channels.world-map.main", channel);
+    }
+
+    /**
+     * Set the minimap channel name in the configuration.
+     * @param channel The minimap channel name to set.
+     */
+    public void setMinimapChannel(String channel) {
+        write("channels.minimap.main", channel);
+    }
+
+    /**
+     * Determine if a new server ID should be generated.
+     *
+     * Note: This will reload the configuration automatically. No need to call reloadResource() before.
+     * @return True if the current server ID is 0, false otherwise.
+     */
+    public boolean isGenerateNewId() {
+        int currentId = getXaeroServerId();
+        return currentId == 0;
+    }
+
+    /**
+     * Check if a new server ID should be generated and generate one if necessary.
+     * Note: This will reload the configuration automatically. No need to call reloadResource() before.
+     */
+    public void checkAndGenerateNewId() {
+        if (! isGenerateNewId()) return;
+
+        int newId = getRandomServerId();
+        setServerId(newId);
+    }
+
+    /**
+     * Generate a random server ID between -1,000,000 and 1,000,000.
+     * @return The generated server ID.
+     */
     public int getRandomServerId() {
         Random random = ThreadLocalRandom.current();
 
-        return random.nextInt(1, 1000000);
+        return random.nextInt(-1000000, 1000000);
     }
 }
--- src/main/java/gg/drak/xaerosupport/utils/XaeroUtils.java
+++ src/main/java/gg/drak/xaerosupport/utils/XaeroUtils.java
@@ -7,53 +7,94 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class XaeroUtils {
-    public static void sendServerId(Player player, String channel, int serverLevelId) {
+    /**
+     * Sends the server ID to the player on the specified channel.
+     * @param player The player to send the message to.
+     * @param channel The channel to send the message on.
+     * @param serverId The server ID to send.
+     */
+    public static void sendServerId(Player player, String channel, int serverId) {
         ByteArrayDataOutput bytes = ByteStreams.newDataOutput();
         bytes.writeByte(0);
-        bytes.writeInt(serverLevelId);
+        bytes.writeInt(serverId);
 
         player.sendPluginMessage(XaeroSupport.getInstance(), channel, bytes.toByteArray());
     }
 
+    /**
+     * Sends the server ID to the player on the specified channel.
+     * @param player The player to send the message to.
+     * @param channel The channel to send the message on.
+     */
     public static void sendServerId(Player player, String channel) {
         sendServerId(player, channel, getServerId());
     }
 
+    /**
+     * Sends the server ID to the player on both the world map and minimap channels.
+     * @param player The player to send the message to.
+     */
     public static void sendWithBoth(Player player) {
         XaeroUtils.sendServerId(player, getWorldMapChannel());
         XaeroUtils.sendServerId(player, getMinimapChannel());
     }
 
+    /**
+     * Registers the outgoing plugin channels for world map and minimap.
+     */
     public static void registerChannels() {
         XaeroSupport.getInstance().logInfo("Registering channels...");
         registerChannel(getWorldMapChannel());
         registerChannel(getMinimapChannel());
     }
 
+    /**
+     * Unregisters the outgoing plugin channels for world map and minimap.
+     */
     public static void unregisterChannels() {
         XaeroSupport.getInstance().logInfo("Unregistering channels...");
         unregisterChannel(getWorldMapChannel());
         unregisterChannel(getMinimapChannel());
     }
 
+    /**
+     * Registers an outgoing plugin channel.
+     * @param channel The channel to register.
+     */
     public static void registerChannel(String channel) {
         Bukkit.getMessenger().registerOutgoingPluginChannel(XaeroSupport.getInstance(), channel);
         XaeroSupport.getInstance().logInfo("Registered channel: &c" + channel);
     }
 
+    /**
+     * Unregisters an outgoing plugin channel.
+     * @param channel The channel to unregister.
+     */
     public static void unregisterChannel(String channel) {
         Bukkit.getMessenger().unregisterOutgoingPluginChannel(XaeroSupport.getInstance(), channel);
         XaeroSupport.getInstance().logInfo("Unregistered channel: &c" + channel);
     }
 
+    /**
+     * Gets the world map channel from the configuration.
+     * @return The world map channel.
+     */
     public static String getWorldMapChannel() {
         return XaeroSupport.getMainConfig().getWorldMapChannel();
     }
 
+    /**
+     * Gets the minimap channel from the configuration.
+     * @return The minimap channel.
+     */
     public static String getMinimapChannel() {
         return XaeroSupport.getMainConfig().getMinimapChannel();
     }
 
+    /**
+     * Gets the server ID from the configuration.
+     * @return The server ID.
+     */
     public static int getServerId() {
         return XaeroSupport.getMainConfig().getXaeroServerId();
     }
